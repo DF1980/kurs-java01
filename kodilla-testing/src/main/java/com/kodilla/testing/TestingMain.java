65b40f5e2937a0d2a203541727dd7a6991c1e0c2
Dariusz Fudziński <git@github.com:DF1980/kurs-java01.git>
2025-03-19 20:28:59 +0100

    6.1 Added kodilla-testing module,

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,27 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-        public static void main(String[] args) {
-            System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+    public static void main(String[] args) {
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
         }
 
+        Calculator calculator = new Calculator();
+
+        int resultAdd = calculator.add(5, 3);
+        System.out.println("Test add(5, 3): " + (resultAdd == 8 ? "Test ok" : "Failed"));
+
+        int resultSubtract = calculator.subtract(10, 4);
+        System.out.println("Test subtract(10, 4): " + (resultSubtract == 6 ? "Test ok" : "Failed"));
+    }
 }
